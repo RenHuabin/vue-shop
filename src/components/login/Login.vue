c8095a0f7fee59d4fdf1e8134da94649f365ac27
renhuabin <renhuabin@123.cn>
2021-08-04 11:14:01 +0800

    dajian2

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,16 +1,14 @@
 <template>
-  <div id="app">
+  <div id="login">
+    登录组件
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
